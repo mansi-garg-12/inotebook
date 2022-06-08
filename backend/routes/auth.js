05dbc4a73beadeecfa48fa4107b52f60af0c0173
Mansi <gargmansi0307@gmail.com>
2022-06-08 19:28:05 +0530

    added  post

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,8 @@
 const express=require('express');
 const router=express.Router();
 const User=require('../modules/User');
-router.get('/',(req,res)=>{
-    console.log("hii");
-    res.send("hii");
+router.post('/',(req,res)=>{
+  
      console.log(req.body);
     const user = User(req.body);
     user.save()
