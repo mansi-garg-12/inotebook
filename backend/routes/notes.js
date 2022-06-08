466c28d8e1e8c3ca37627082997994e862be6acc
Mansi <gargmansi0307@gmail.com>
2022-06-08 13:53:46 +0530

    connected to backend

---
+++ backend/routes/notes.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const router=express.Router();
+const User=require('../modules/Notes');
+router.get('/',(req,res)=>{
+    console.log("hii");
+    res.send("hii");
+     console.log(req.body);
+    const user = User(req.body);
+    user.save()
+    res.send(req.body);
+})
+module.exports=router
