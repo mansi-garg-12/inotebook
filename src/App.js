668a44d2557fd1724a777cffe7b227b73e794611
Mansi <gargmansi0307@gmail.com>
2022-06-18 23:57:40 +0530

    added notes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React from 'react'
+import React from "react";
 
-
-import Home from './components/Home'
-import About from './components/About'
-import Login from './components/Login'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import NoteState from './context/notes/NoteState'
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import NoteState from "./context/notes/NoteState";
+import Alert from "./components/Alert";
 
 const App = () => {
   return (
-      <>
-        <NoteState>
-          <Router>
-              <Navbar/>
-              <div className="container">
-              <Switch>
-                  <Route exact path="/"><Home /></Route>
-                  <Route exact path="/about"><About /></Route>
-                  <Route exact path="/Login"><Login/></Route>
-              </Switch>
-              </div>
-          </Router>
-          </NoteState>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert message="you can do it mansi !!!! weldone" />
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/Login">
+                <Login />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
+    </>
+  );
+};
 
-
-
-      </>
-  )
-}
-
-export default App
+export default App;
